b1ba83ec7500e9f56c58f13a7c0f7dae9f9af199
Amandeep <73280299+ads3342@users.noreply.github.com>
2021-09-08 19:45:42 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
-btnTranslate.addEventListener("click", function call(){
-var a=prompt("Hey! Dont Worry We are listening to the click events.");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+//btnTranslate.addEventListener("onclick", function call(){
+// var a=prompt("Hey! Dont Worry We are listening to the click events.");
 
-console.log("you enterd " + txtInput.value );
-})
-//console.log(btnTranslate);
+// console.log("you enterd " + txtInput.value );
+// })
+// //console.log(btnTranslate);
+
+function clickHandler(){
+    console.log("you enterd " + txtInput.value )
+    txtOutput.innerText= ("You Entered !! "+txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
+
